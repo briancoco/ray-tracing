3f5830d9936e82b59a3a449beb8803dc5b1e8731
briancoco <briannguyencoco@gmail.com>
2024-04-22 18:11:50 -0500

    fixed bp lighting

--- src/Sphere.cpp
+++ src/Sphere.cpp
@@ -70,8 +70,8 @@ public:
 		glm::vec3 ca = ambient;
 		glm::vec3 n = calcNorm(x);
 		for (size_t i = 0; i < lights.size(); i++) {
-			glm::vec3 l = glm::normalize(lights[i]->position - position);
-			glm::vec3 e = glm::normalize(glm::vec3(0, 0, 5) - position);
+			glm::vec3 l = glm::normalize(lights[i]->position - x);
+			glm::vec3 e = glm::normalize(glm::vec3(0, 0, 5) - x);
 			glm::vec3 h = glm::normalize(l + e);
 
 			glm::vec3 cd = diffuse * glm::max(0.0f, glm::dot(l, n));
